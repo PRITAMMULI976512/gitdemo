6f4073a25e76e9853711293be550497d69450599
PRITAMMULI <pritammuli@gmail.com>
2026-01-04 10:58:57 +0530

    Signup partially completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Signup from './component/Signup';
 
 function App() {
   return (
  <div>
     <h1>Welcome to Git Demo Application - from Priatm branch</h1>
+
+    <Signup />
  </div>
   );
 }
--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -4,6 +4,13 @@ function Signup() {
   return (
     <div>
       <h1>Signup Component - from Pratikdsha branch</h1>
+
+      <form action="">
+        <input type="text" placeholder='Enter your name' /><br /><br />
+        <input type="email" placeholder='Enter your email' /><br /><br />
+        <input type="password" placeholder='Enter your password' /><br /><br />
+        <button type="submit">Signup</button>
+      </form>
     </div>
   )
 }
